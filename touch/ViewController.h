09ea5742370fb6588fccb622e287fbc50cc48c0e
西林咲音 <s.n.baton319@icloud.com>
2015-04-22 19:57:53 +0900

    fastcommit

--- touch/ViewController.h
+++ touch/ViewController.h
@@ -9,7 +9,31 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    UIImage *imgArray[4];
+    IBOutlet UIImageView *imageView;
+    IBOutlet UIButton *stamp1;
+    IBOutlet UIButton *stamp2;
+    IBOutlet UIButton *stamp3;
+    IBOutlet UIButton *stamp4;
+    
+    int index;
+    UIImageView *stamps[100];
+    int stampNum;
+    
+    BOOL traceMode;
+    float tranceNumber;
+    
+    IBOutlet UIImageView *stampBaseView;
+}
 
-
+-(IBAction)tapButton1;
+-(IBAction)tapButton2;
+-(IBAction)tapButton3;
+-(IBAction)tapButton4;
+-(IBAction)clear;
+-(IBAction)back;
+-(IBAction)kaiten;
+-(IBAction)kaitenkaijo;
 @end
 
--- touch/ViewController.m
+++ touch/ViewController.m
@@ -17,11 +17,82 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    imgArray[0] = [UIImage imageNamed:@"massu2.png"];
+    imgArray[1] = [UIImage imageNamed:@"onji2.png"];
+    imgArray[2] = [UIImage imageNamed:@"hazime2.png"];
+    imgArray[3] = [UIImage imageNamed:@"falcon2.png"];
+    UIImage *image = [UIImage imageNamed:@"IMG_3367.png"];
+    [imageView setImage:image];
+    traceMode = NO;
+    tranceNumber = 45.0;
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    //if(index > 0){
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self.view];
+    UIImageView *imgView = [[UIImageView alloc]initWithImage:imgArray[index]];
+    imgView.center = CGPointMake(location.x, location.y);
+    CGFloat angle = tranceNumber * M_PI / 180.0;
+    imgView.transform = CGAffineTransformMakeRotation(angle);
+    //traceMode = NO;
+    if (location.y > 406) {
+        
+    }else{
+        [self.view addSubview:imgView];
+    }
+    
+    stamps[stampNum] = imgView;
+    stampNum += 1;
+    //    if()
+    //    {
+    //        UIImage *image = [UIImage imageNamed:@"];
+    //    }
+    //}
+}
+-(IBAction)tapButton1{
+    index = 0;
+    tranceNumber = 0;
+}
+-(IBAction)tapButton2{
+    index = 1;
+    tranceNumber = 0;
+}
+-(IBAction)tapButton3{
+    index = 2;
+    tranceNumber = 0;
+}
+-(IBAction)tapButton4{
+    index = 3;
+    tranceNumber = 0;
+}
+-(IBAction)back{
+    if(stampNum > 0){
+        stampNum -= 1 ;
+        [stamps[stampNum] removeFromSuperview];
+        stamps[stampNum] = nil;
+    }
+}
+
+-(IBAction)clear{
+    while(stampNum > 0){
+        stampNum -= 1 ;
+        [stamps[stampNum] removeFromSuperview];
+        stamps[stampNum] = nil;
+    }
+}
+-(void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)kaiten{
+    tranceNumber = tranceNumber + 22.5;
+    
+}
+
+-(IBAction)kaitenkaijo{
+    tranceNumber = 0;
 }
 
 @end
